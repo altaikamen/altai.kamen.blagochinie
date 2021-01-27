3fff6647de7a5f09575516e9e803ade2adb47d6d
Серафим <70561906+ItYaS@users.noreply.github.com>
2021-01-27 20:27:00 +0700

    Add files via upload

--- app/article.py
+++ app/article.py
@@ -25,18 +25,22 @@ class DataBase:
         table = """
                     CREATE TABLE IF NOT EXISTS articles (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        title TEXT,
+                        link TEXT,
+                        preview TEXT,
                         article TEXT
                     );
                 """
 
         self.update_data('articles.sqlite', table)
 
-        for article in [str('статья ' + str(elem)) for elem in range(1, 50)]:
+        for i in [str('статья ' + str(elem)) for elem in range(1, 50)]:
+            article = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac."
             query = f"""
                         INSERT INTO
-                            articles (article)
+                            articles (title, link, preview, article)
                         VALUES
-                            ('{article}');
+                            ('{i}', '{i}', '{article}', '{article}');
                     """
 
             self.update_data('articles.sqlite', query)
--- app/views.py
+++ app/views.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template
 from app import random_quote as rq
 from app import article as a
+from icecream import ic
 
 app = Flask(__name__)
 
 all_articles = a.get_article()
 last_article_id = all_articles[-1][0]
-
-first_elem_index = 0
-last_elem_index = 10
 
 first_articles = {'first_id': 0, 'last_id': 10, 'last_index': last_article_id, 'min': True, 'max': False}
 now_articles = {}
@@ -69,63 +67,88 @@ def saints():
     return render_template('dev_page.html', quote=rq.get_random_quote())
 
 
+@app.route('/<string:post>/')
+def watch_post(post):
+    post = {'title': 'КакойНибудьЗаголовок', 'article': 'КакБыПримерСтатьи'}
+    return render_template('post.html', quote=rq.get_random_quote(), post=post)
+
+
 @app.route('/page<int:id>/')
 def next_page(id):
     global now_articles
 
-    print(id)
-
-    """if id == 0:
-        now_articles = {'first_id': 0, 'last_id': 10, 'last_index': last_article_id, 'id': 1, 'min': True, 'max': False}
-        first_id = 0
-        last_id = 10
-
-        return render_template('index.html', quote=rq.get_random_quote(), posts=all_articles[first_id:last_id],
-                               now_articles=now_articles)"""
+    # для самой первой страницы
     if id == 1:
-        first_id = 11
+        first_id = 10
         last_id = 20
 
-        now_articles = {'first_id': first_id, 'last_id': last_id, 'last_index': last_article_id, 'id': 2, 'min': False, 'max': False}
-        print(now_articles)
+        id += 1
 
-        return render_template('index.html', quote=rq.get_random_quote(), posts=all_articles[first_id:last_id],
+        now_articles = {'first_id': first_id, 'last_id': last_id, 'last_index': last_article_id, 'next_id': id,
+                        'min': False, 'max': False}
+
+        return render_template('index.html', quote=rq.get_random_quote(),
+                               posts=all_articles[first_id:last_id],
                                now_articles=now_articles)
 
+    # для последующих страниц
     first_id = now_articles['first_id'] + 10
     last_id = first_id + 10
 
-    id = str(first_id)[0]
+    id += 1
 
+    # для самой последней страницы
     if last_id >= last_article_id:
-        if first_id < last_article_id:
-            first_id = last_article_id - 10
+        first_id = last_article_id - 10
 
-            now_articles = {'first_id': first_id, 'last_id': last_article_id, 'last_index': last_article_id,
-                            'min': False, 'max': True}
+        id = str(last_article_id)[0]
+        id = int(id)
 
-            return render_template('index.html', quote=rq.get_random_quote(),
-                                   posts=all_articles[first_id:last_article_id],
-                                   now_articles=now_articles)
+        now_articles = {'first_id': first_id, 'last_id': last_article_id, 'last_index': last_article_id, 'next_id': id,
+                        'min': False, 'max': True}
+
+        return render_template('index.html', quote=rq.get_random_quote(),
+                               posts=all_articles[first_id:last_id],
+                               now_articles=now_articles)
     else:
-        now_articles = {'first_id': first_id, 'last_id': last_id, 'last_index': last_article_id, 'id': int(id),
+        # для последующих страниц
+        now_articles = {'first_id': first_id, 'last_id': last_id, 'last_index': last_article_id, 'next_id': id,
                         'min': False, 'max': False}
-
-        print(now_articles)
 
         return render_template('index.html', quote=rq.get_random_quote(),
                                posts=all_articles[first_id:last_id],
                                now_articles=now_articles)
 
 
-@app.route('/last_page/')
-def last_page():
-    global first_elem_index, last_elem_index
+@app.route('/page<int:id>/')
+def last_page(id):
+    global now_articles
 
-    first_elem_index -= 10
-    last_elem_index -= 10
+    first_id = now_articles['first_id'] - 10
+    last_id = now_articles['last_id'] - 10
 
-    if first_elem_index <= 0:
-        return render_template('index.html', quote=rq.get_random_quote(), posts=all_articles[0:10])
+    if first_id <= 0:
+        # для самой первой страницы
+        first_id = 1
+        last_id = 11
+        id = 1
+
+        now_articles = {'first_id': first_id, 'last_id': last_id, 'last_index': last_article_id, 'id': id,
+                        'min': False, 'max': False}
+        ic(now_articles)
+
+        return render_template('index.html', quote=rq.get_random_quote(),
+                               posts=all_articles[first_id:last_id],
+                               now_articles=now_articles)
     else:
-        return render_template('central_more.html', quote=rq.get_random_quote(), posts=all_articles[first_elem_index:last_elem_index])
+        # для последующих страниц
+        id -= 1
+
+        now_articles = {'first_id': first_id, 'last_id': last_id, 'last_index': last_article_id, 'id': id,
+                        'min': False, 'max': False}
+
+        ic(now_articles)
+
+        return render_template('index.html', quote=rq.get_random_quote(),
+                               posts=all_articles[first_id:last_id],
+                               now_articles=now_articles)
